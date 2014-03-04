00a17c0630102aba54569a7ab64f59e3de3f3b26
Pirkka Jokela <pirkka.jokela@gmail.com>
2014-03-04 23:34:54 +0200

    You can now extract URLs from a flow.

---
+++ src/scala/main/flowexit/Http.scala
@@ -0,0 +1,54 @@
+package flowexit
+
+import java.net._
+import scala.io._
+
+/**
+ * https://user:pass@api.flowdock.com/flows/:org/:flow/messages/:message_id
+ */
+case class Http(user : String, password : String, org : String, flow : String) {
+  def get(requestUrl : String) : String = {
+    val url = new URL(requestUrl)
+    val connection = url.openConnection().asInstanceOf[HttpURLConnection]
+    connection.setRequestProperty("Authorization", authorizationHeader);
+    if (connection.getResponseCode() != 200)
+      throw new IllegalStateException("Response code: " + connection.getResponseCode() + " from url: " + url);
+    val source = Source.fromInputStream(connection.getInputStream())("UTF-8")
+    source.getLines.mkString("\n")
+  }
+  
+  /**
+   * https://user:pass@api.flowdock.com/flows/:org/:flow/messages/:message_id
+   * GET /flows/:organization/:flow/messages
+   */
+  def listMessages() = {
+    val url = s"https://api.flowdock.com/flows/$org/$flow/messages"
+    System.err.println("[META] GET: " + url)
+    get(url)
+  }
+  
+  def listMessages(untilId : String) = {
+    val url = s"https://api.flowdock.com/flows/$org/$flow/messages?until_id=$untilId"
+    System.err.println("[META] GET: " + url)
+    get(url)
+  }
+  
+  def post(requestUrl : String, params : Map[String, String]) : String = {
+    val url = new URL(requestUrl)
+    val connection = url.openConnection().asInstanceOf[HttpURLConnection]
+    val source = Source.fromInputStream(connection.getInputStream())("UTF-8")
+    if (connection.getResponseCode() != 200) {
+      throw new IllegalStateException("Response code: " + connection.getResponseCode() + " from url: " + url + "\n" + source.getLines.mkString("\n"));
+    }
+    source.getLines.mkString("\n")
+  }
+  
+  /*
+   * conn.setRequestProperty("Authorization", basicAuth);
+   */
+  def authorizationHeader() = {
+    val userpass = user + ":" + password
+    val basicAuth = "Basic " + javax.xml.bind.DatatypeConverter.printBase64Binary(userpass.getBytes())
+    basicAuth
+  }
+} 
---
+++ src/scala/main/flowexit/Json.scala
@@ -0,0 +1,26 @@
+package flowexit
+
+import scala.util.parsing.json._
+
+object Json {
+  def parseJson(str : String) = 
+    Json(JSON.parseRaw(str).get)
+}
+
+case class Json(raw : Any) {
+  def array = raw.asInstanceOf[JSONArray].list.map(Json(_))
+  
+  def prop(name : String) : Json = {
+    val jsObj = raw.asInstanceOf[JSONObject]
+    val jsMap = jsObj.obj
+    Json(jsMap(name))
+  }
+  
+  def str = raw.toString
+  def double = raw.asInstanceOf[Double]
+  
+  def isProp(name : String) = isObj && raw.asInstanceOf[JSONObject].obj.contains(name)
+  def isObj = raw.isInstanceOf[JSONObject]
+  def isStr = raw.isInstanceOf[String]
+  def isArray = raw.isInstanceOf[JSONArray]
+}
